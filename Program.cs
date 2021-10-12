fe79627774f15d26f996480e5878caa3c8a40d64
HorvathRichardd <91724138+HorvathRichardd@users.noreply.github.com>
2021-10-12 11:33:58 +0200

    10.12

--- Program.cs
+++ Program.cs
@@ -4,62 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace elágazások
+namespace kozosora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            feladat16();
             Console.ReadLine();
+            feladat1();
         }
-        static void feladat16()
+        static void feladat1()
         {
-            Console.WriteLine("Válasszon egy érdemjegyet");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            int jegy = int.Parse(Console.ReadLine());
-
-            switch (jegy)
+            for (int i = 0; i < 21; i++)
             {
-                case 1:
-                    
-                    Console.WriteLine("Elégtelen");
 
-                    break;
-
-                case 2:
-
-                    Console.WriteLine("Elégséges");
-
-                    break;
-
-                case 3:
-
-                    Console.WriteLine("közepes");
-
-                    break;
-
-                case 4:
-
-                    Console.WriteLine("jó");
-
-                    break;
-
-                case 5:
-
-                    Console.WriteLine("jeles");
-
-                    break;
-
-                default:
-                    Console.WriteLine("Nem létező osztályzat");
-                    break;
+                Console.Write("{0} ", i);
             }
+            Console.WriteLine();
+            for (int i = 0; i < 21; i++)
+            {
 
+                Console.WriteLine("{0} ", i);
+            }
         }
     }
+
 }
